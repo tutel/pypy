e5065039561292c2a5135e1d442123b9686549e8
unknown <chuck891023@gmail.com>
2019-10-02 11:27:51 +0800

    my first commit

---
+++ poem.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+from numpy.random import randint
+from numpy.random import choice
+words='''
+我
+我的
+妳
+妳的
+心
+溫柔
+日子
+雨
+風
+天空
+雲
+等待
+哭泣
+戀愛
+相遇
+分離
+忘記
+心醉
+驀然
+吹過
+思念
+靈魂
+停止'''
+for i in range(a):
+    b = randint(3,5)
+    ch = choice(phrase,b)
+    C = ''.join(ch)
+    print(C)
